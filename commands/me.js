7f804ec1000f1108ecaea4f655449e323f5a4a5e
henrychu04 <henrychu04@outlook.com>
2021-06-11 17:40:37 -0400

    added user info command

---
+++ commands/me.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+exports.run = async (client, message, args) => {
+  let userArray = await User.find({ d_id: message.author.id });
+  let user = userArray[0];
+
+  const userInfoEmbed = new Discord.MessageEmbed()
+    .setColor('#7756fe')
+    .setTitle(`${user.username} information`)
+    .addFields(
+      { name: 'USD Amount', value: user.usdAmount },
+      { name: 'Free USD Amount', value: user.freeUsdAmount },
+      { name: 'Portfolio Value', value: user.portfolioValue }
+    );
+
+  await message.channel.send(userInfoEmbed).then(console.log(`${message} completed\n`));
+};
